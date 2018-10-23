7b0e0b6d1d347030067ce722b7eafd8cb8838bd3
Auston Hefling <auston.hefling@outlook.com>
2018-10-23 08:00:07 -0400

    Add New project

---
+++ ContosoUniversityData/Student.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Student] 
+(
+	[StudentID]			int identity(1, 1) not null,
+	[LastName]			nvarchar(50)           null,
+	[FirstName]			nvarchar(50)		   null,
+	[EnrollmentDate]	datetime			   null,
+	primary key clustered ([StudentID] asc)
+)
