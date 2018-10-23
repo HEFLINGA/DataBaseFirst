bd994f6053a54240b8afb8e91272027c0a3136de
Auston Hefling <auston.hefling@outlook.com>
2018-10-23 09:11:58 -0400

    Finish Exercise

---
+++ ContosoSite/Models/PartialClasses.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace ContosoSite.Models
+{
+    [MetadataType(typeof(StudentMetaData))]
+    public partial class Student
+    {
+    }
+
+    [MetadataType(typeof(EnrollmentMetaData))]
+    public partial class Enrollment
+    {
+    }
+}
